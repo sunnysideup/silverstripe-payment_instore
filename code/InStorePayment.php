0577fa633b4a1aa63305edea35e084abcfc4b0c3
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2010-11-18 22:43:38 +0000

    separation of instore payment
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/payment_instore/trunk@349 950b781c-c544-efab-2f99-90083ee69683

---
+++ code/InStorePayment.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Payment object representing an In Store Payment (order online and pick-up in store).
+ * @author Nicolaas [at] sunnysideup.co.nz
+ * @package payment
+ */
+class InStorePayment extends ChequePayment {
+
+				protected static $custom_message_for_in_store_payment = "";
+								static function set_custom_message_for_in_store_payment($v) {self::$custom_message_for_in_store_payment = $v;}
+
+				/**
+				 * Process the In Store payment method
+				 */
+				function processPayment($data, $form) {
+								$this->Status = 'Pending';
+								if(!self::$custom_message_for_in_store_payment) {
+												$page = DataObject::get_one("CheckoutPage");
+												if($page) {
+																self::$custom_message_for_in_store_payment = $page->ChequeMessage;
+												}
+								}
+								$this->Message = self::$custom_message_for_in_store_payment;
+								$this->write();
+								return new Payment_Success();
+				}
+
+				function getPaymentFormFields() {
+								return new FieldSet(
+												new HiddenField("InStore", "InStore", 0)
+								);
+				}
+
+				function getPaymentFormRequirements() {
+								return null;
+				}
+
+}
+
