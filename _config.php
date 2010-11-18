e1d814bd98358181dc2262a70018712d3736cc27
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2010-11-18 22:44:03 +0000

    separation of instore payment
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/payment_instore/trunk@350 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -6,4 +6,5 @@
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START payment_instore MODULE ----------------===================
+//InStorePayment::set_custom_message_for_in_store_payment("To improve security, we would like you to pay for your purchase when you pick-up your products in our store.");
 //===================---------------- END payment_instore  MODULE ----------------===================
