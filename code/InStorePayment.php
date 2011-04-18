61c912a6a9fe58e3f99964c187713a1c573c3c75
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-04-18 03:51:06 +0000

    fixed identation
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/payment_instore/trunk@1075 950b781c-c544-efab-2f99-90083ee69683

--- code/InStorePayment.php
+++ code/InStorePayment.php
@@ -7,34 +7,34 @@
  */
 class InStorePayment extends ChequePayment {
 
-				protected static $custom_message_for_in_store_payment = "";
-								static function set_custom_message_for_in_store_payment($v) {self::$custom_message_for_in_store_payment = $v;}
+	protected static $custom_message_for_in_store_payment = "";
+		static function set_custom_message_for_in_store_payment($v) {self::$custom_message_for_in_store_payment = $v;}
 
-				/**
-				 * Process the In Store payment method
-				 */
-				function processPayment($data, $form) {
-								$this->Status = 'Pending';
-								if(!self::$custom_message_for_in_store_payment) {
-												$page = DataObject::get_one("CheckoutPage");
-												if($page) {
-																self::$custom_message_for_in_store_payment = $page->ChequeMessage;
-												}
-								}
-								$this->Message = self::$custom_message_for_in_store_payment;
-								$this->write();
-								return new Payment_Success();
-				}
+	/**
+	 * Process the In Store payment method
+	 */
+	function processPayment($data, $form) {
+		$this->Status = 'Pending';
+		if(!self::$custom_message_for_in_store_payment) {
+			$page = DataObject::get_one("CheckoutPage");
+			if($page) {
+				self::$custom_message_for_in_store_payment = $page->ChequeMessage;
+			}
+		}
+		$this->Message = self::$custom_message_for_in_store_payment;
+		$this->write();
+		return new Payment_Success();
+	}
 
-				function getPaymentFormFields() {
-								return new FieldSet(
-												new HiddenField("InStore", "InStore", 0)
-								);
-				}
+	function getPaymentFormFields() {
+		return new FieldSet(
+			new HiddenField("InStore", "InStore", 0)
+		);
+	}
 
-				function getPaymentFormRequirements() {
-								return null;
-				}
+	function getPaymentFormRequirements() {
+		return null;
+	}
 
 }
 
