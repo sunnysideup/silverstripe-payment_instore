106bfda41bfaeb4e576cc528e935cca265162aac
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-29 13:48:10 +1300

    PATCH: module clean-up

--- code/InStorePayment.php
+++ code/InStorePayment.php
@@ -5,29 +5,30 @@
  * @author Nicolaas [at] sunnysideup.co.nz
  * @package payment
  */
-class InStorePayment extends EcommercePayment {
+class InStorePayment extends EcommercePayment
+{
+    private static $custom_message_for_in_store_payment = "";
 
-	private static $custom_message_for_in_store_payment = "";
+    /**
+     * Process the In Store payment method
+     */
+    public function processPayment($data, $form)
+    {
+        $this->Status = 'Pending';
+        $this->Message = Config::inst()->get("InStorePayment", "custom_message_for_in_store_payment");
+        $this->write();
+        return EcommercePayment_Success::create();
+    }
 
-	/**
-	 * Process the In Store payment method
-	 */
-	function processPayment($data, $form) {
-		$this->Status = 'Pending';
-		$this->Message = Config::inst()->get("InStorePayment", "custom_message_for_in_store_payment");
-		$this->write();
-		return EcommercePayment_Success::create();
-	}
+    public function getPaymentFormFields()
+    {
+        return new FieldList(
+            new HiddenField("InStore", "InStore", 0)
+        );
+    }
 
-	function getPaymentFormFields() {
-		return new FieldList(
-			new HiddenField("InStore", "InStore", 0)
-		);
-	}
-
-	function getPaymentFormRequirements() {
-		return null;
-	}
-
+    public function getPaymentFormRequirements()
+    {
+        return null;
+    }
 }
-
